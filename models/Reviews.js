262bf8bbdb4c10c6773a0cf289ea66098c72395f
calvin <puram.calvin@gmail.com>
2020-02-06 12:44:50 +0100

    created Reviews model

---
+++ models/Reviews.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ReviewsSchma = new Schema({
+  review: {
+    type: String,
+    required: [true, 'a review is required']
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true
+  },
+  tour: {
+    type: Schema.Types.ObjectId,
+    ref: 'tours',
+    required: true
+  }
+});
+
+const Reviews = mongoose.model('Reviews', ReviewsSchma);
+module.exports = Reviews;
