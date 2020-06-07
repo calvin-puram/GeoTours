33dfe9a6fea4267de778c59dbd5aab47ec21bfa9
calvin <puram.calvin@gmail.com>
2020-06-07 14:48:13 +0100

    test get all tours route

---
+++ __test__/integration/tours/getTours.spec.js
@@ -0,0 +1,85 @@
+/**
+ * @jest-environment node
+ */
+
+/* eslint-disable no-undef */
+// eslint-disable-next-line node/no-unpublished-require
+const supertest = require('supertest');
+
+const server = require('../../../app');
+
+const app = () => supertest(server);
+const DB = require('../../utils/db');
+const Tours = require('../../../models/Tours');
+const Users = require('../../../models/Users');
+
+describe('The get single tour process', () => {
+  const user = {
+    name: 'calvin',
+    email: 'cpuram1@gmail.com',
+    password: '2begood4',
+    passwordConfirm: '2begood4',
+    role: 'admin'
+  };
+
+  const data = [
+    {
+      name: 'The Forest Hiker',
+      duration: 5,
+      maxGroupSize: 25,
+      difficulty: 'easy',
+      ratingsAverage: 4.7,
+      ratingsQuantity: 37,
+      price: 397,
+      summary: 'Breathtaking hike ',
+      description: 'Ut enim ad minim ',
+      imageCover: 'tour-1-cover.jpg',
+      images: ['tour-1-1.jpg', 'tour-1-2.jpg', 'tour-1-3.jpg'],
+      startDates: ['2021-04-25,10:00', '2021-07-20,10:00', '2021-10-05,10:00']
+    },
+
+    {
+      name: 'The Net Ninja',
+      duration: 5,
+      maxGroupSize: 25,
+      difficulty: 'easy',
+      ratingsAverage: 4.7,
+      ratingsQuantity: 37,
+      price: 397,
+      summary: 'Breathtaking hike ',
+      description: 'Ut enim ad minim ',
+      imageCover: 'tour-1-cover.jpg',
+      images: ['tour-1-1.jpg', 'tour-1-2.jpg', 'tour-1-3.jpg'],
+      startDates: ['2021-04-25,10:00', '2021-07-20,10:00', '2021-10-05,10:00']
+    }
+  ];
+
+  let admin;
+
+  beforeEach(async () => {
+    await DB.connectDB();
+    admin = await Users.create(user);
+    await Tours.create(data);
+    request = (url, token) => {
+      return app()
+        .get(url)
+        .set('authorization', `Bearer ${token}`);
+    };
+  });
+
+  it('should get single tour with given id', async () => {
+    const token = await admin.sendJWT();
+    const allTours = await Tours.find();
+    const res = await request(`/api/v1/tours/`, token);
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.data.length).toBe(allTours.length);
+  });
+
+  afterEach(async () => {
+    await Tours.deleteMany();
+    await Users.deleteMany();
+    await DB.closeDB();
+  });
+});
